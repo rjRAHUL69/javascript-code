cf6a2ebdbefb019adc7058bb1b805454c219e224
Rahul70 <128919698+rjRAHUL69@users.noreply.github.com>
2024-10-21 14:28:33 +0000

    message

---
+++ 01_basics/variable.js
@@ -0,0 +1,25 @@
+const accoutId=12344
+let accountemail="mahato44@gmailcom"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+console.log(accountemail)
+console.log(accountPassword)
+console.log(accountCity)
+
+
+accountemail="rahul@123gmail.com"
+accountPassword="54321"
+accountCity="Bengalore"
+
+console.log(accountemail) //this is the one way to print something
+console.log(accountPassword)
+console.log(accountCity)
+
+//Another way to print something
+
+console.table([accoutId,accountemail,accountPassword,accountCity,accountState])
+
+
+
