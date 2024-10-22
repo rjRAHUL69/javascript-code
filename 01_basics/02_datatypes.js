83e50b79bd5781b0c92992f3b7c1f0420302ed8b
Rahul70 <128919698+rjRAHUL69@users.noreply.github.com>
2024-10-22 01:39:55 +0000

    push commit here

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all JS code as newer version
+
+//alert ( 3 + 3 )//we are using nodejs . not browser . here the output is error
+
+
+//it is not the way to write the code 
+//The whole code is write like , easily read
+/*console.log(3
+    + 
+    3
+)*/
+
+console.log("rahul")
+
+
+//variable types
+/* two ty
+
+
+let name="Rahul"//string variable
+let age = 18 //number variable
+let isLoggedIn = false
