a3363d7d846947e75b81531a4f272f636e67a8e9
Rahul70 <128919698+rjRAHUL69@users.noreply.github.com>
2024-10-24 13:56:15 +0000

    date and time

---
+++ 01_basics/06_date_and_time.js
@@ -0,0 +1,10 @@
+//dates
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+
+
+
