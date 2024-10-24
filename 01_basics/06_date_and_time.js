209ce132fb09b5f6636e5bc944fb0d44fcafd2bc
Rahul70 <128919698+rjRAHUL69@users.noreply.github.com>
2024-10-24 15:49:30 +0000

    date and time

--- 01_basics/06_date_and_time.js
+++ 01_basics/06_date_and_time.js
@@ -1,10 +1,33 @@
 //dates
 let myDate = new Date();
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleDateString());
-console.log(typeof myDate);
+//console.log(myDate.toString());//op is universal time
+//console.log(myDate.toDateString());//print date 
+//console.log(myDate.toLocaleDateString());
+//console.log(typeof myDate);//object
+
+//let myCreatedDate = new Date(2023,0 ,23);
+//console.log(myCreatedDate.toDateString());//Mon Jan 23 2023
+
+//let myCreatedDate = new Date(2023,0 ,23,5,3);
+// console.log(myCreatedDate.toLocaleString());//1/23/2023, 5:03:00 AM
+
+let myCreatedDate = new Date("01-14-2023");
+//console.log(myCreatedDate.toLocaleString());//1/14/2023, 12:00:00 AM
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);//1729784227704
+console.log(myCreatedDate.getTime());//1673654400000
+console.log(Math.floor(Date.now()/1000));//1729784482
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDate());
 
 
-
+//method that use randomly
+newDate.toLocaleString('default',{
+    weekday: "long",
+    
+})
 
