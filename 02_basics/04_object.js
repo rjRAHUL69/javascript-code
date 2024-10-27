324f10406effd2cd4b6c146f73b19e818b375ccb
Rahul70 <128919698+rjRAHUL69@users.noreply.github.com>
2024-10-27 11:46:19 +0000

    object

---
+++ 02_basics/04_object.js
@@ -0,0 +1,16 @@
+const course = {
+    coursename: "js in hindi",
+    price:"999",
+    courseInstructor : "Rahul"
+}
+//course.courseInstructor
+const {courseInstructor:instructor} =course;
+//console.log(courseInstructor);
+console.log(instructor);
+
+/*What is json format
+it is object notation.it use {}
+object use []
+
+api web name:random user me api
+tool: json formatter=>paste the code =>format/beautiffy=>convert tree structure*/
