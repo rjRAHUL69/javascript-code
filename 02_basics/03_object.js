d7f0b0c6c18aec468ab7708b98aa6c66412a0250
Rahul70 <128919698+rjRAHUL69@users.noreply.github.com>
2024-10-27 00:50:40 +0000

    object

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -30,7 +30,7 @@ const regularUser = {
 
    // const obj3 = { obj1,obj2}
    //console.log(obj3);//op is { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
-   const obj3 = Object.assign(obj1,obj2)
-   console.log(obj3);//op { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+   //const obj3 = Object.assign(obj1,obj2)
+   //console.log(obj3);//op { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
     
