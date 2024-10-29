0026b2e90b2dd50e8dccb39e1884c2bfb7e72a95
Rahul70 <128919698+rjRAHUL69@users.noreply.github.com>
2024-10-29 18:55:58 +0000

    arrow

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,50 @@
+// let a = 10 ;
+// const b = 20;
+// var c = 30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+/* scopes
+1.global scopes
+    work globally
+2.local scopes
+    work in the braces
+    use let variable,if use var then it not work ex:
+
+ these are like global and local variable
+*/
+// let a =300; //global variable
+// if (true) {
+//     let a = 10 ;
+// const b = 20;
+// var c = 30;
+    
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+/*
+
+function one() {
+    const username = "Rahul";
+
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+
+     console.log(website);
+    two()
+}
+one()
+
+*/
+
+
+
+
---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,8 @@
+const user = {
+    username: "Rahul",
+    price: 999,
+    welcomeMessage:function(){
+        console.log(`${this.username} , Welcome to website`);
+    
+    }
+}
