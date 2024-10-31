1fd24797c9cd4450f3c471a143ac1afc6647ae33
Rahul70 <128919698+rjRAHUL69@users.noreply.github.com>
2024-10-31 01:46:36 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,8 +1,31 @@
-const user = {
-    username: "Rahul",
-    price: 999,
-    welcomeMessage:function(){
-        console.log(`${this.username} , Welcome to website`);
+// const user = {
+//     username: "Rahul",
+//     price: 999,
+//     welcomeMessage:function(){
+//         console.log(`${this.username} , Welcome to website`);
     
-    }
+//     }
+// }
+//user.welcomeMessage();//op is Rahul , Welcome to website
+
+//user.username = "sam";//now we cahange the username
+
+//user.welcomeMessage();//op is sam , Welcome to website, because here this function change  the current context
+ 
+
+//if this keyword define in the function
+//function chai(){
+   // let username ="Rahul";
+   // console.log(this.username);//op: undefined
+    
+// }
+
+//chai();
+
+
+//now arrow functiion
+const chai = function() {
+    let username = "Rahul"
+    console.log(this.username);
+    
 }
