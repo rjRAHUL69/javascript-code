4bff7c83462d2777ea7ef9e3921ef68f189fddc0
Rahul70 <128919698+rjRAHUL69@users.noreply.github.com>
2024-11-08 03:40:58 +0000

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,16 @@
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element) ;
+    
+    
+// }
+
+
+//while loop
+let myArray = [ 'flash' , "Batman" , "Superman"];
+
+let arr = 0 ;
+while(arr < myArray.length){
+    console.log(`the hero is ${myArray[arr]}`)
+    arr = arr + 1
+}
---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+//for of and for in loop 
+
+//for of
+//string  in array= ["","",""]
+//object in array = [{},{},{}]
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for(const num of arr){
+//     console.log(num);
+    
+// }
+
+
+const greetings = "Hello World";
+for (const greet  of greetings) {
+   // console.log(`Each char is ${greet}`);
+    
+}
+
+
+//maps
+const map = new Map()
+map.set('IN',"india")
+map.set('USA',"United States of America")
+map.set('Fr',"France")
+console.log(map)
